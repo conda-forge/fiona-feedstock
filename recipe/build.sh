6728aa260bba9777b6f261a9501c766774c2e1b6
ocefpaf <ocefpaf@gmail.com>
2016-04-04 06:42:58 -0300

    Fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
- if [ $(uname) == Darwin ]; then
-  brew remove --force $(brew list)
-fi
-
 $PYTHON setup.py build_ext -I$PREFIX/include -L$PREFIX/lib -lgdal install --single-version-externally-managed --record record.txt
