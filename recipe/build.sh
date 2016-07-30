a802c4e4d08c824a0958d589775e1f8d25769bed
ocefpaf <ocefpaf@gmail.com>
2016-07-30 20:51:36 -0300

    More tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ $(uname) == Darwin ]]; then
-  export LDFLAGS="$LDFLAGS -headerpad_max_install_names"
+  export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
 
-$PYTHON setup.py build_ext -I$PREFIX/include -L$PREFIX/lib -lgdal install --single-version-externally-managed --record record.txt
+$PYTHON setup.py build_ext -I$PREFIX/include -L$PREFIX/lib -lgdal \
+                 install --single-version-externally-managed --record record.txt
