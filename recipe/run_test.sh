711538921de5485d0074448ea435831ddad39c39
ocefpaf <ocefpaf@gmail.com>
2017-02-06 12:39:43 -0300

    Argh.... OS X

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,8 @@
+# OS X is broken.
+# See: https://travis-ci.org/conda-forge/fiona-feedstock/jobs/198841996
+if [[ $(uname) == Linux ]]; then
 mkdir -p tests/tests/data
 cp test_data/coutwildrnp.zip tests/tests/data
 
 nosetests --exclude test_filter_vsi --exclude test_geopackage --exclude test_write_mismatch tests
+fi
