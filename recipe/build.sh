2e3289f98bd2a8839eb723a4ea12dcbf9cdd5f7f
Filipe Fernandes <ocefpaf@gmail.com>
2017-12-15 20:06:03 -0200

    test LANG in Travis-CI

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 if [[ $(uname) == Darwin ]]; then
   export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
+  export LANG=en_US.UTF-8
 fi
 
 $PYTHON setup.py build_ext -I$PREFIX/include -L$PREFIX/lib -lgdal \
