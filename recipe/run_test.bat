e3bddfa53d927bfd42f31bfa7e148730b8643542
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-06-14 17:41:02 +0200

    skip test_no_append_driver_cannot_append[PCIDSK]

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -4,4 +4,4 @@ xcopy test_data\coutwildrnp.tar tests\data /s /e /y || exit 1
 xcopy test_data\coutwildrnp.json tests\data /s /e /y || exit 1
 
 :: The tests are finishing but it hangs at the end :-/
-REM pytest -s -rxs -v -k "not (test_fio_ls_single_layer or test_directory or test_directory_trailing_slash or test_options or test_transaction)" tests
+REM pytest -s -rxs -v -k "not (test_fio_ls_single_layer or test_directory or test_directory_trailing_slash or test_options or test_transaction or test_no_append_driver_cannot_append[PCIDSK])" tests
