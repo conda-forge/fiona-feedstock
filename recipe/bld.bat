905db8a34319c82c45b455df90d3362a5c8f6f78
akrherz <akrherz@iastate.edu>
2021-11-29 21:48:27 -0600

    take one more swing at windows bld

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,10 +5,5 @@ set GDAL_VERSION=%%F
 if errorlevel 1 exit 1
 echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-"%PYTHON%" setup.py build_ext ^
-                    --inplace ^
-                    -I"%LIBRARY_INC%" ^
-                    -lgdal_i ^
-                    -L"%LIBRARY_LIB%"
-"%PYTHON%" -m pip install --no-deps --ignore-installed .
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
