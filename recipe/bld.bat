94555fa49af28e47f4687d27d75e5e0617b38e9b
akrherz <akrherz@iastate.edu>
2021-11-29 20:31:17 -0600

    try rasterio's way of build_ext on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,9 @@ set GDAL_VERSION=%%F
 if errorlevel 1 exit 1
 echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgdal_i"
+"%PYTHON%" setup.py build_ext ^
+                    -I"%LIBRARY_INC%" ^
+                    -lgdal_i ^
+                    -L"%LIBRARY_LIB%" install ^
+                    --single-version-externally-managed
 if errorlevel 1 exit 1
