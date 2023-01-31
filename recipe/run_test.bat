4eb83fe622984f04a4d6fb022929b1227384ed36
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-01-31 09:30:03 +0100

    also skip listdir tests on windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,4 +3,4 @@ xcopy test_data\coutwildrnp.zip tests\data /s /e /y || exit 1
 xcopy test_data\coutwildrnp.tar tests\data /s /e /y || exit 1
 xcopy test_data\coutwildrnp.json tests\data /s /e /y || exit 1
 
-%PYTHON% -m pytest -s -rxs -v -m "not wheel" -k "not (test_fio_ls_single_layer or test_directory or test_directory_trailing_slash or test_options or test_transaction or test_no_append_driver_cannot_append[PCIDSK])" tests
+%PYTHON% -m pytest -s -rxs -v -m "not wheel" -k "not (test_fio_ls_single_layer or test_directory or test_directory_trailing_slash or test_options or test_transaction or test_no_append_driver_cannot_append[PCIDSK] or test_listdir_path or test_listdir_zipmemoryfile)" tests
