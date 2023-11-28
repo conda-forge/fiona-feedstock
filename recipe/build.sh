f8477d02de5dfa77823013431f0367d9c79dabdc
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-11-27 22:09:33 -0700

    Simplify unix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-if [[ $(uname) == Darwin ]]; then
-  export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
-fi
-
-$PYTHON -m pip install --no-deps --ignore-installed . \
-                       --global-option=build_ext \
-                       --global-option="-I$PREFIX/include" \
-                       --global-option="-L$PREFIX/lib" \
-                       --global-option="-lgdal"
+which gdal-config
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
